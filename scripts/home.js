8bcc1088dfe7915bc1e66f4bc53c3c55abb25afd
Samuel Mercado <smercadoperez67@gmail.com>
2024-09-26 17:42:06 -0400

    new file:   chamber-of-commerce-site-plan.html 	new file:   chamber/directory.html 	modified:   index.html 	new file:   scripts/church-members.json 	modified:   scripts/home.js 	new file:   styles/site-plan.css

--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,6 @@
 const filterButtons = document.querySelectorAll('.filter-buttons button');
-const courseButton = document.querySelectorAll('.course.btn');
+// const courseButton = document.querySelectorAll('.course.btn');
+const course = document.querySelector(".courses"); 
 
 
 filterButtons.forEach( button => {
@@ -10,8 +11,31 @@ filterButtons.forEach( button => {
 });
 
 
+const buttons = [
+    { id: 1, label: 'CSE 110', className: 'course-button', category: 'CSE' },
+    { id: 2, label: 'CSE 111', className: 'course-button', category: 'CSE' },
+    { id: 3, label: 'WDD 130', className: 'course-button', category: 'WDD' },
+    { id: 4, label: 'WDD 131', className: 'course-button', category: 'WDD' },
+    { id: 5, label: 'WDD 231', className: 'course-button', category: 'WDD' }
+];
+
+
+buttons.forEach(buttonObj =>{
+    const newButton = document.createElement('button');
+    newButton.textContent = buttonObj.label;
+    newButton.classList.add(buttonObj.className)
+});
+// Limpiar el div, y crear elementos de una lista 
+// 1:Crear un array que contiene las 6 courses 
+
+// 2: crear una funion que cree botones, 
+// 3: btn= clase y category.
+//4: loop de los contenidos de la lista
 function filterCourses(filter){
+    console.log(filter);
+    console.log(courseButton);
     courseButton.forEach(course => {
+        
         if(filter=='all'){
             course.computedStyleMap.display = 'block';
         } else if (course.getAttribute('data-category') == filter){
