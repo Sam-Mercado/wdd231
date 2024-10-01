683d670faf6ee24bec4c97a38606084c668c304c
Samuel Mercado <smercadoperez67@gmail.com>
2024-09-30 23:07:02 -0400

    modified:   chamber/directory.html 	modified:   chamber/scripts/directory.js 	modified:   chamber/styles/directory.css

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -6,6 +6,18 @@ hamburgerElement.addEventListener('click',() =>{
     navElement.classList.toggle('open');
 });
 
+//year
+const year = document.querySelector('#year');
+const today = new Date();
+
+year.innerHTML = `<span class='highlight'>${today.getFullYear()}</span>&copy; Samuel Mercado Perez`;
+
+const option = {year:'numeric', month:'long', day:'numeric'};
+const lastUpdated = today.toLocaleDateString('en-US', option);
+
+document.getElementById('last-update').textContent = `Last Update ${lastUpdated}`;
+
+
 
 //where are we pulling the information from 
 //and where do we want to display it
@@ -37,3 +49,4 @@ const displayBusiness = (busineses) =>{
         
     });
 }
+
