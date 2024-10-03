cd5c9c146f140d8ae640af0fe2cff13a1fcd14f7
Samuel Mercado <smercadoperez67@gmail.com>
2024-10-03 10:56:44 -0400

    modified:   week03/Consuming APIs/apihomework.js 	new file:   week03/Consuming APIs/response.js 	new file:   week03/Consuming APIs/response.text

--- week03/Consuming APIs/apihomework.js
+++ week03/Consuming APIs/apihomework.js
@@ -1,15 +1,23 @@
+const indexMain = document.querySelector('main');
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const url = 'https://api.openweathermap.org/data/3.0/onecall/overview?lat=49.759654069316746&lon=6.644248775359994&appid=6a8fc707a9dba6c994341f21d5ac9fe5';
+
+const myKey ='ffe61aadd59c22f5b4f49d97471830f4';
+const myLat ='49.759654069316746';
+const myLon ='6.644248775359994';
+
+
+const myUrl = `//api.openweathermap.org/data/2.5/?lat=${myLat}&lon=${myLon}&appid=${myKey}`;
 
 
 async function apiFetch(){
     try {
-        const response = await fetch(url);
+        const response = await fetch(myUrl);
         if (response.ok){
             const data = await response.json();
             console.log(data);
+            displayResults(data);
         } else {
             throw Error(await response.text());
         } 
@@ -19,4 +27,16 @@ async function apiFetch(){
 
 };
 
+function displayResults(d){
+    indexMain.innerHTML = d.name;
+    captionDesc.innerHTML=d.weather[0].description;
+    currentTemp.innerHTML=`${d.main.temp}&deg;F`;
+
+    const iconsrc = `https://openweathermap.org/img/wn/${d.weather[0].icon}@2x.png`;
+    weatherIcon.setAttribute('SRC', iconsrc);
+    weatherIcon.setAttribute('alt', data.weather[0].description);
+
+}
+
 apiFetch();
+
