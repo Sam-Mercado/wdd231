88609240c846400523eb232cd2777bcadba1bcd9
Samuel Mercado <smercadoperez67@gmail.com>
2024-10-21 19:25:31 -0400

    week06

--- EnglishInstitute/scripts/general.js
+++ EnglishInstitute/scripts/general.js
@@ -2,3 +2,24 @@ const year = document.querySelector("#copy");
 const currentYear = new Date().getFullYear();
 year.textContent = currentYear
 
+//footer
+
+async function getSocial(){
+    let responce = await fetch('./data/cardsInformation.json');
+    let data = await responce.json();
+
+    data.social.forEach(element => {
+        let card = document.createElement('div');
+        card.classList.add('content-flex-row');
+        let socialImg = document.createElement('img');
+
+        socialImg.src = `${element.image}`;
+        socialImg.loading = "lazy";
+
+        card.appendChild(socialImg);
+
+        document.querySelector('#social').appendChild(card);
+    });
+}
+
+getSocial();
